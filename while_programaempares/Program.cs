eec01bc94408c14bf790ae9d1b2b6ed0c52064fc
Usuario <Usuario@CPD03-M11>
2024-03-26 10:47:00 -0300

    Adicionar arquivos de projeto.

---
+++ while_programaempares/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_programaempares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int cont = 0;
+            while (cont >= 0 && cont < 101 )
+            {
+               
+                    Console.WriteLine(cont);
+                    cont+=2;
+             
+            }
+            Console.ReadKey();
+        }
+    }
+}
